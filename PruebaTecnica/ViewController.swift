ac6d66b675da292185f5ca16f5905b35b2d773f3
Jose Antonio <joseantonioaravena@gmail.com>
2020-03-25 06:12:46 -0300

    Initial Commit

---
+++ PruebaTecnica/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PruebaTecnica
+//
+//  Created by Jose Antonio Aravena on 25-03-20.
+//  Copyright © 2020 Jose Antonio Aravena. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
